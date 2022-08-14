55f6f0a5870cbc005ce9a8477e7635e3021ac2d8
huanamigo <arturfurtak5@gmail.com>
2022-08-14 09:30:15 +0200

    css bg change

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const username = document.querySelector("#username");
-const password = document.querySelector("#password");
-const password2 = document.querySelector("#password2");
-const email = document.querySelector("#email");
+const username = document.querySelector('#username');
+const password = document.querySelector('#password');
+const password2 = document.querySelector('#password2');
+const email = document.querySelector('#email');
 
-const resetBtn = document.querySelector(".clear");
-const sendBtn = document.querySelector(".send");
-const popup = document.querySelector(".popup");
+const resetBtn = document.querySelector('.clear');
+const sendBtn = document.querySelector('.send');
+const popup = document.querySelector('.popup');
 
 const checkForm = (input) => {
   input.forEach((el) => {
-    if (el.value == "") {
+    if (el.value == '') {
       showError(el, el.placeholder);
     } else {
       clearError(el);
@@ -23,61 +23,64 @@ const checkMail = (email) => {
   if (re.test(email.value)) {
     clearError(email);
   } else {
-    showError(email, "Email nie jest poprawny");
+    showError(email, 'E-mail is not correct');
   }
 };
 
 const showError = (input, msg) => {
   const formBox = input.parentElement;
-  const errorMsg = formBox.querySelector(".error-text");
+  const errorMsg = formBox.querySelector('.error-text');
 
   errorMsg.textContent = msg;
-  formBox.classList.add("error");
+  formBox.classList.add('error');
 };
 
 const clearError = (input) => {
   const formBox = input.parentElement;
-  formBox.classList.remove("error");
+  formBox.classList.remove('error');
 };
 
 const checkLength = (input, min) => {
   if (input.value.length < min) {
     let label = input.previousElementSibling.innerText;
-    showError(input, `${label.split(":")[0]} składa się z min ${min} znaków.`);
+    showError(
+      input,
+      `${label.split(':')[0]} must be at least ${min} characters long.`
+    );
   }
 };
 
 const checkPass = () => {
   if (password.value !== password2.value) {
-    showError(password2, "Hasła są różne");
+    showError(password2, "Passwords don't match");
   }
 };
 
 const checkErrors = () => {
-  const allInputs = document.querySelectorAll(".form-box");
+  const allInputs = document.querySelectorAll('.form-box');
   let errorCount = 0;
 
   allInputs.forEach((el) => {
-    if (el.classList.contains("error")) {
+    if (el.classList.contains('error')) {
       errorCount++;
     }
   });
 
   if (errorCount == 0) {
-    popup.classList.add("show-popup");
+    popup.classList.add('show-popup');
   }
 };
 
-resetBtn.addEventListener("click", (e) => {
+resetBtn.addEventListener('click', (e) => {
   e.preventDefault();
 
   [username, password, password2, email].forEach((el) => {
-    el.value = "";
+    el.value = '';
     clearError(el);
   });
 });
 
-sendBtn.addEventListener("click", (e) => {
+sendBtn.addEventListener('click', (e) => {
   e.preventDefault();
 
   checkForm([username, password, password2, email]);
